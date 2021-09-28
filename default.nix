f0e89ffe1941399cc4d6428875a3dbf4c3e1f1cd
renesat <smol.ivan97@gmail.com>
2021-09-28 22:00:03 +0300

    Add datalad

--- default.nix
+++ default.nix
@@ -7,6 +7,7 @@ let
   packages = {
     autorestic = pkgs.callPackage ./pkgs/autorestic { };
     activitywatch-bin = pkgs.callPackage ./pkgs/activitywatch-bin { };
+    datalad = pkgs.callPackage ./pkgs/datalad { };
   };
   supportedSystem = (name: pkg: builtins.elem system pkg.meta.platforms);
 in (filterAttrs supportedSystem packages)
