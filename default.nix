085ead7f20526ff86648a636463caa648c60b942
renesat <smol.ivan97@gmail.com>
2021-09-28 21:05:35 +0300

    Init repo

---
+++ default.nix
@@ -0,0 +1,12 @@
+{ system ? builtins.currentSystem, pkgs ? import <nixpkgs> { inherit system; }
+}:
+
+with pkgs;
+with lib;
+let
+  packages = {
+    autorestic = pkgs.callPackage ./pkgs/autorestic { };
+    activitywatch-bin = pkgs.callPackage ./pkgs/activitywatch-bin { };
+  };
+  supportedSystem = (name: pkg: builtins.elem system pkg.meta.platforms);
+in (filterAttrs supportedSystem packages)
