16fc217f9ec0f9d7ee1989b873839af07ed39a25
renesat <smol.ivan97@gmail.com>
2023-08-02 01:50:37 +0200

    Minor fixes

--- pkgs/daqp/python.nix
+++ pkgs/daqp/python.nix
@@ -15,7 +15,9 @@ python3.pkgs.buildPythonPackage rec {
     hash = "sha256-in7Ci/wM7i0csJ4XVfo1lboWOyfuuU+8E+TzGmMV3x0=";
   };
 
-  propagatedBuildInputs = with python3.pkgs; [cython unittestCheckHook];
+  nativeBuildInputs = with python3.pkgs; [unittestCheckHook];
+
+  propagatedBuildInputs = with python3.pkgs; [cython ];
 
   postPatch = ''
     sed -i 's|../../../daqp|../..|' interfaces/daqp-python/setup.py
