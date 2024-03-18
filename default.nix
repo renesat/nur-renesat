5e2c542f67ae84261af2bb7890b4dcca8776e5f2
renesat <smol.ivan97@gmail.com>
2024-03-18 08:37:40 +0100

    Clean default.nix

--- default.nix
+++ default.nix
@@ -16,14 +16,12 @@
     hledger-utils = pkgs.callPackage ./pkgs/hledger-utils {};
     drawilleplot = pkgs.callPackage ./pkgs/drawilleplot {};
     drawille = pkgs.callPackage ./pkgs/drawille {};
-    # stash = pkgs.callPackage ./pkgs/stash/ui.nix { };
     math-preview = pkgs.callPackage ./pkgs/math-preview {};
     daqp = pkgs.callPackage ./pkgs/daqp {};
     daqp-python = pkgs.callPackage ./pkgs/daqp/python.nix {};
     qpsolvers = pkgs.callPackage ./pkgs/qpsolvers {daqp = daqp-python;};
     normcap = pkgs.callPackage ./pkgs/normcap {};
     taskwarrior3 = pkgs.callPackage ./pkgs/taskwarrior3 {};
-    # vegafusion = pkgs.callPackage ./pkgs/vegafusion {};
 
     vl-convert-python = pkgs.python3Packages.callPackage ./pkgs/vl-convert-python {};
     vegafusion-python-embed = pkgs.python3Packages.callPackage ./pkgs/vegafusion/vegafusion-python-embed.nix {};
