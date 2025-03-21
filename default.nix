b76a05ce0fc7977ed4c614f2f25f4e9b3ae3bd16
renesat <self@renesat.me>
2025-03-21 09:27:37 +0100

    fix: lazy warn for aliases
    Not show warn before installPhase

--- default.nix
+++ default.nix
@@ -7,22 +7,27 @@
     },
 }: let
   inherit (builtins) warn;
+  lazyPackageWarn = msg: pkg:
+    pkg.overrideAttrs (prev: {
+      installPhase = warn msg prev.installPhase;
+    });
+  supportedSystem = _: pkg: builtins.elem system pkg.meta.platforms;
   aliases = {
-    autorestic = warn "Please use `autorestic` from nixpkgs: `pkgs.autorestic`" pkgs.autorestic;
-    datalad = warn "Please use `datalad` from nixpkgs: `pkgs.datalad`" pkgs.datalad;
-    warpd = warn "Please use `warpd` from nixpkgs: `pkgs.warpd`" pkgs.warpd;
-    taskwarrior3 = warn "Please use `taskwarrior3` from nixpkgs: `pkgs.taskwarrior3`" pkgs.taskwarrior3;
-    "1fps" = warn "Please use `1fps` from nixpkgs: `pkgs._1fps`" pkgs._1fps;
-    puffin = warn "Please use `puffin` from nixpkgs: `pkgs.puffin`" pkgs.puffin;
-    math-preview = warn "Please use `math-preview` from nixpkgs: `pkgs.math-preview`" pkgs.math-preview;
-    activitywatch-bin = warn "Please use `activitywatch-bin` from nixpkgs: `pkgs.activitywatch`" pkgs.activitywatch;
-    daqp-python = warn "Please use `daqp-python` from nixpkgs: `pkgs.python3Packages.daqp`" pkgs.python3Packages.daqp;
-    drawille = warn "Please use `drawille` from nixpkgs: `pkgs.python3Packages.drawille`" pkgs.python3Packages.drawille;
-    drawilleplot = warn "Please use `drawilleplot` from nixpkgs: `pkgs.python3Packages.drawilleplot`" pkgs.python3Packages.drawilleplot;
-    hledger-utils = warn "Please use `hledger-utils` from nixpkgs: `pkgs.hledger-utils`" pkgs.hledger-utils;
-    normcap = warn "Please use `normcap` from nixpkgs: `pkgs.normcap`" pkgs.normcap;
-    qpsolvers = warn "Please use `qpsolvers` from nixpkgs: `pkgs.python3Packages.qpsolvers`" pkgs.python3Packages.qpsolvers;
-    questdb = warn "Please use `questdb` from nixpkgs: `pkgs.questdb`" pkgs.questdb;
+    autorestic = lazyPackageWarn "Please use `autorestic` from nixpkgs: `pkgs.autorestic`" pkgs.autorestic;
+    datalad = lazyPackageWarn "Please use `datalad` from nixpkgs: `pkgs.datalad`" pkgs.datalad;
+    warpd = lazyPackageWarn "Please use `warpd` from nixpkgs: `pkgs.warpd`" pkgs.warpd;
+    taskwarrior3 = lazyPackageWarn "Please use `taskwarrior3` from nixpkgs: `pkgs.taskwarrior3`" pkgs.taskwarrior3;
+    "1fps" = lazyPackageWarn "Please use `1fps` from nixpkgs: `pkgs._1fps`" pkgs._1fps;
+    puffin = lazyPackageWarn "Please use `puffin` from nixpkgs: `pkgs.puffin`" pkgs.puffin;
+    math-preview = lazyPackageWarn "Please use `math-preview` from nixpkgs: `pkgs.math-preview`" pkgs.math-preview;
+    activitywatch-bin = lazyPackageWarn "Please use `activitywatch-bin` from nixpkgs: `pkgs.activitywatch`" pkgs.activitywatch;
+    daqp-python = lazyPackageWarn "Please use `daqp-python` from nixpkgs: `pkgs.python3Packages.daqp`" pkgs.python3Packages.daqp;
+    drawille = lazyPackageWarn "Please use `drawille` from nixpkgs: `pkgs.python3Packages.drawille`" pkgs.python3Packages.drawille;
+    drawilleplot = lazyPackageWarn "Please use `drawilleplot` from nixpkgs: `pkgs.python3Packages.drawilleplot`" pkgs.python3Packages.drawilleplot;
+    hledger-utils = lazyPackageWarn "Please use `hledger-utils` from nixpkgs: `pkgs.hledger-utils`" pkgs.hledger-utils;
+    normcap = lazyPackageWarn "Please use `normcap` from nixpkgs: `pkgs.normcap`" pkgs.normcap;
+    qpsolvers = lazyPackageWarn "Please use `qpsolvers` from nixpkgs: `pkgs.python3Packages.qpsolvers`" pkgs.python3Packages.qpsolvers;
+    questdb = lazyPackageWarn "Please use `questdb` from nixpkgs: `pkgs.questdb`" pkgs.questdb;
   };
   packages = rec {
     toutui = pkgs.callPackage ./pkgs/toutui {};
@@ -44,6 +49,5 @@
       inherit vl-convert;
     };
   };
-  supportedSystem = _: pkg: builtins.elem system pkg.meta.platforms;
 in
   pkgs.lib.filterAttrs supportedSystem (packages // aliases)
